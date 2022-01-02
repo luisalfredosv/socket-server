922a129b85d5c1366dccc6b97bd6f60fbe547ac9
luisalfredosv <luissarabia80@gmail.com>
2022-01-01 20:36:43 -0400

    actualizacion de Class Server

--- classes/server.ts
+++ classes/server.ts
@@ -1,16 +1,70 @@
 import express, { Application } from "express";
+
 import { SERVER_PORT } from "../global/environment";
 
+import { Server as SocketServer } from "socket.io";
+import { createServer, Server as HttpServer } from "http";
+
+import * as socket from "../sockets/socket";
+import cors, { CorsOptions } from "cors";
+import { router } from "../routes/router";
+
 export default class Server {
+	private static _instance: Server;
+
 	public app: Application;
 	public port: number;
 
-	constructor() {
+	public io: SocketServer;
+	private httpServer: HttpServer;
+
+	private constructor() {
 		this.app = express();
 		this.port = SERVER_PORT;
+
+		this.setMiddleware();
+		this.httpServer = createServer(this.app);
+		this.io = new SocketServer(this.httpServer, {
+			cors: {
+				origin: "*",
+			},
+		});
+		this.setRoutes();
+		this.listenSockets();
+	}
+
+	private setMiddleware() {
+		this.app.use(express.json());
+		this.app.use(express.urlencoded({ extended: true }));
+		const restCors: CorsOptions = {
+			origin: "*",
+		};
+		this.app.use(cors(restCors));
+	}
+
+	private setRoutes() {
+		this.app.use("/", router);
+	}
+
+	public static get instance() {
+		return this._instance || (this._instance = new this());
+	}
+
+	private listenSockets() {
+		console.log("Escuchando conexiones");
+
+		this.io.on("connection", (client) => {
+			console.log("Cliente conectado");
+
+			// Disconnect
+			socket.disconnect(client);
+
+			// Message
+			socket.message(client, this.io);
+		});
 	}
 
 	start(callback: Function) {
-		this.app.listen(this.port, callback());
+		this.httpServer.listen(this.port, callback());
 	}
 }
