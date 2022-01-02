4e1b197dd044907f7ac0e9885ac81e311e56ef55
luisalfredosv <luissarabia80@gmail.com>
2022-01-01 20:37:14 -0400

    Importar Server Class y ejecutar

--- index.ts
+++ index.ts
@@ -1,19 +1,5 @@
 import Server from "./classes/server";
-import { router } from "./routes/router";
-import express from "express";
-import cors from "cors";
-
-const server = new Server();
-
-server.app.use(express.json());
-server.app.use(express.urlencoded({ extended: true }));
-
-server.app.use(cors({
-	origin: true,
-	credentials: true
-}))
-
-server.app.use('/', router);
+const server = Server.instance;
 
 server.start(() => {
 	console.log(`Server started on port ${server.port}`);
